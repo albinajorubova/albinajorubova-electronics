cc6c71195437bea006b872051ade56d3f08a9bf8
Jorubova Albina <deletawer@gmail.com>
2022-12-19 20:37:04 +0300

    first commit

---
+++ assets/tabs.js
@@ -0,0 +1,25 @@
+const tabsBtn = document.querySelectorAll(".iphone_navlinks");
+const tabsItems = document.querySelectorAll(".iphone_navlink-item");
+
+tabsBtn.forEach(onTabClick);
+
+function onTabClick(item) {
+  item.addEventListener("click", function () {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute("data-tab");
+    let currentTab = document.querySelector(tabId);
+
+    if (!currentBtn.classList.contains("active")) {
+      tabsBtn.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      tabsItems.forEach(function (item) {
+        item.classList.remove("active");
+      });
+
+      currentBtn.classList.add("active");
+      currentTab.classList.add("active");
+    }
+  });
+}
