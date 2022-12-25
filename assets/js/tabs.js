159dd160c7193de7834694f322b76f71951f50cf
Jorubova Albina <deletawer@gmail.com>
2022-12-25 19:46:26 +0300

    fetch&sortList

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -1,6 +1,6 @@
 const tabsBtn = document.querySelectorAll(".iphone_navlinks");
 const tabsItems = document.querySelectorAll(".iphone_navlink-item");
-
+const container = document.querySelector(".footer_container");
 tabsBtn.forEach(onTabClick);
 
 function onTabClick(item) {
@@ -24,15 +24,34 @@ function onTabClick(item) {
   });
 }
 
-const container = document.querySelector(".footer_container");
+const tabSix = document.querySelector(".tab-6");
+const popularReview = document.querySelector(".product_popularreview");
+const recentlyWatched = document.querySelector(".iphone_recentlywatched");
+tabSix.addEventListener("click", () => {
+  container.style.position = "relative";
+  container.style.top = "3800px";
+  popularReview.style.display = "none";
+
+  recentlyWatched.style.position = "absolute";
+  recentlyWatched.style.top = "400px";
+});
+
 const tabTwo = document.querySelector(".tab-2");
 tabTwo.addEventListener("click", () => {
   container.style.position = "relative";
   container.style.top = "4100px";
+  popularReview.style.display = "flex";
+
+  recentlyWatched.style.position = " absolute";
+  recentlyWatched.style.top = "900px ";
 });
 
 const tabOne = document.querySelector(".tab-1");
 tabOne.addEventListener("click", () => {
   container.style.position = "absolute";
   container.style.top = "2300px";
+  popularReview.style.display = "flex";
+
+  recentlyWatched.style.position = "absolute";
+  recentlyWatched.style.top = "900px";
 });
